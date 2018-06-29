82bea31c4b55eefa3358b70b614dcfc2f7032d39
ic3w0lf22 <ic3w0lf22@gmail.com>
2018-06-28 20:22:57 -0600

    Add files via upload

---
+++ ROBLOX_MULTI.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <Windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	CreateMutex(NULL, TRUE, L"ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
+
+	printf("You can now use multiple ROBLOX clients. Closing this will cause all ROBLOX clients to terminate except for one.\n");
+	printf("Press any key to close");
+	getch();
+	return 0;
+}
