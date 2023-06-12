82f95b3e04f77dd98708788262cb94108ead904a
catnotadog <84086733+catnotadog@users.noreply.github.com>
2023-06-12 10:16:14 -0400

    Update ROBLOX_MULTI.cpp
    fix shit lol :p

--- ROBLOX_MULTI.cpp
+++ ROBLOX_MULTI.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	CreateMutex(NULL, TRUE, L"ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
+	CreateMutex(NULL, TRUE, L"ROBLOX_singletonEvent"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
 
 	printf("You can now use multiple ROBLOX clients. Closing this will cause all ROBLOX clients to terminate except for one.\n");
 	printf("Press any key to close");
